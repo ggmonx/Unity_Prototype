6ef7ea14ed47cd9cb076a817762774e27c8334a8
ggmonx <gaston.garrido98@gmail.com>
2022-04-03 09:24:13 -0700

    add teleporter and 2nd scene and fix levels

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private GameObject enemy;
     void Start()
     {
     }
